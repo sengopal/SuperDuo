c1f1e6e9b3273558ad735f6d28c82512629fa191
sengopal <senthil777@gmail.com>
2015-10-01 09:38:50 -0700

    fixing comments

--- alexandria/app/src/main/java/it/jaschke/alexandria/AddBook.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/AddBook.java
@@ -85,7 +85,8 @@ public class AddBook extends Fragment implements LoaderManager.LoaderCallbacks<C
                     ean="978"+ean;
                 }
                 if(ean.length()<13){
-                    clearFields();
+                    //Consider removing this call to clearFields() here, so that if a user accidentally removes an ISBN number the book information does not disappear.
+                    //clearFields();
                     return;
                 }
                 //Once we have an ISBN, start a book intent
